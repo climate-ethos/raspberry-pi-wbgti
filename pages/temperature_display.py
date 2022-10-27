acf60596e99fec65b8d31289dc104e3064cc9d1b
Connor Forbes <connorforbes25@gmail.com>
2022-10-27 13:52:22 +1000

    feat: Update sensor labels

--- pages/temperature_display.py
+++ pages/temperature_display.py
@@ -14,18 +14,21 @@ from modules.read_digital_probe_temp import get_temperature_values
 class TemperatureDisplay(tk.Frame):
 	ax = None
 	ani = None
+	sensorLabels = []
+	wbgtLabel = None
 	last_wbgt_readings = collections.deque([np.nan] * 10, maxlen=10)
 
 	def __init__(self, parent, controller) -> None:
 		tk.Frame.__init__(self, parent)
 		# Temperature sensor display
 		for i in range(3):
-			label = tk.Label(self, text="Sensor {}:\n{}℃".format(i+1, 35.27), borderwidth=1, relief="solid", padx=10, pady=10)
+			label = tk.Label(self, borderwidth=1, relief="solid", padx=10, pady=10)
 			label.grid(column=i, row=0)
+			self.sensorLabels.append(label)
 
 		# WBGTI display
-		wbgtiDisplay = tk.Label(self, text="WBGT:\n{}℃".format(35.27), borderwidth=1, relief="solid", padx=10, pady=10)
-		wbgtiDisplay.grid(row=0, column=3)
+		self.wbgtLabel = tk.Label(self, borderwidth=1, relief="solid", padx=10, pady=10)
+		self.wbgtLabel.grid(row=0, column=3)
 
 		# Settings button
 		settingsButton = tk.Button(self, text="Settings", command=lambda: controller.show_frame("SettingsPage"))
@@ -44,9 +47,18 @@ class TemperatureDisplay(tk.Frame):
 		self.ani = animation.FuncAnimation(f, self.graph_animate, interval=5000)
 
 	def graph_animate(self, *args):
-		xar = np.arange(-10, 0, 1)
 		sensor_value_dict = get_temperature_values()
-		self.last_wbgt_readings.append(calculate_wbgt(sensor_value_dict))
+		# Update sensor labels
+		for index, item in enumerate(sensor_value_dict.items()):
+			key, value = item
+			self.sensorLabels[index].configure(text="{}:\n{}℃".format(key, value))
+		# Calculate wbgt
+		wbgtValue = calculate_wbgt(sensor_value_dict)
+		# Update wbgt label
+		self.wbgtLabel.configure(text="WBGT:\n{}℃".format(wbgtValue))
+		# Update live graph
+		xar = np.arange(-10, 0, 1)
+		self.last_wbgt_readings.append(wbgtValue)
 		self.ax.clear()
 		self.ax.plot(xar,self.last_wbgt_readings)
 
